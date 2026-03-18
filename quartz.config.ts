a21df9f06d81f980ba2dcef6313ce58cae46b0da
Fawzy Moawad <fawzy3arafa88@gmail.com>
2026-03-17 22:50:46 -0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ethical Hackers Heaven",
+    pageTitle: "The Anubis Protocol",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
